515d9d075718266d02fa7c15574e4cf8fa7c3ee3
deep1229 <54231001+deep1229@users.noreply.github.com>
2020-12-07 14:28:01 +0530

    Update RestController.java

--- ProductMS/src/main/java/com/infy/ekart/controller/RestController.java
+++ ProductMS/src/main/java/com/infy/ekart/controller/RestController.java
@@ -1,5 +1,23 @@
 package com.infy.ekart.controller;
-
+@RestController
+@RequestMapping
 public class RestController {
 
+	
+	@Autowired
+	private ProductService productService;
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@GetMapping(value = "/searchproduct/{productName}")
+	public List<ProductDTO> getProducts(@PathVariable("productName") String productName){
+		logger.info("------getting product details-----");
+		return productService.findProductByName(productName);
+	}
+	@GetMapping(value = "/searchproductbysellerid/{sellerid}")
+	public List<ProductDTO> getProductsBySellerId(@PathVariable("sellerid") Integer seller_id){
+		logger.info("------getting product details-----");
+		return productService.findBySellerId(seller_id);
+	}
 }
+
